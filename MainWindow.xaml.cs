aceba6402bade5a646fed5bacabe60e77e3334e1
MinPt <petr.minailyuk@gmail.com>
2021-09-19 14:39:59 +0300

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFLabies
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(this.MyBox.Text, out var num))
+            {
+                MessageBox.Show(num.ToString());
+            }
+            else
+            {
+                MessageBox.Show(this.MyBox.Text.Length.ToString());
+            } ;
+            
+            
+            
+        }
+    }
+}
