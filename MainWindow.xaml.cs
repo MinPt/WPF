3d5e995f86d5694f50a577e62ed0fe141a19a426
MinPt <petr.minailyuk@gmail.com>
2021-10-03 16:59:30 +0300

    Lab-4 completed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,110 +1,33 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using WPFLabies.Models;
 
-namespace WPF_Labs
+namespace WPFLabies
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
-        private ObservableCollection<Client> _clients = new ObservableCollection<Client>();
+        private readonly Student _student = new Student
+        {
+            FirstName = "Petro",
+            LastName = "Minailiuk",
+            Group = 308,
+            Course = 3
+        };
 
+        public Student Student => _student;
+        
         public MainWindow()
         {
             InitializeComponent();
 
-            SetComboBox();
-
-            clientsTable.DataContext = _clients;
+            DataContext = Student;
         }
 
-        private void SetComboBox()
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            city.ItemsSource = Enum.GetValues(typeof(City)).Cast<City>();
-            city.SelectedValue = City.Kyiv;
-        }
-
-        private void Exit_OnClick(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void AddClient_OnClick(object sender, RoutedEventArgs e)
-        {
-            var client = new Client {FirstName = firstName.Text, LastName = lastName.Text};
-
-            int sitNumber = 0;
-            int flighNum = 0;
-
-            if (!SitIsCorrect(out sitNumber)) return;
-
-            if (!FlightIsCorrect(out flighNum)) return;
-
-            client.SitNumber = sitNumber;
-            client.FlightNumber = flighNum;
-
-            client.TakeOffDate = calendar.SelectedDate ?? DateTime.Today;
-            client.DestinationCity = (City) city.SelectedItem;
-
-            client.Cost = new Random().Next(1500, 2000);
-
-            client.IsVip = vip.IsChecked;
-            if (client.IsVip == true)
-            {
-                client.Cost /= 2;
-            }
-
-            _clients.Add(client);
-        }
-
-        private bool FlightIsCorrect(out int flightNumber)
-        {
-            bool flightIsCorrect = int.TryParse(flightNum.Text, out flightNumber);
-            var i = flightNumber;
-            if (!flightIsCorrect || i < 1 || i > 10)
-            {
-                MessageBox.Show("Flight number is not in range of 1-10! Try again.");
-                return false;
-            }
-
-            return true;
-        }
-
-        private bool SitIsCorrect(out int sitNumber)
-        {
-            
-            bool sitIsCorrect = int.TryParse(sitNum.Text, out sitNumber);
-            var i = sitNumber;
-            var isOcuppied = _clients.FirstOrDefault(c => c.SitNumber == i) != null;
-
-            if ((!sitIsCorrect || isOcuppied) && i < 1 || i > 300)
-            {
-                MessageBox.Show("Sit number is not in range of 1-300 or has already been occupied! Try again.");
-                return false;
-            }
-
-            return true;
-        }
-
-        private void ClearAll_OnClick(object sender, RoutedEventArgs e)
-        {
-            _clients.Clear();
-        }
-
-        private void ClearFields_OnClick(object sender, RoutedEventArgs e)
-        {
-            firstName.Text = "";
-            lastName.Text = "";
-            sitNum.Text = "0";
-            flightNum.Text = "0";
-            vip.IsChecked = false;
-            calendar.SelectedDate = null;
+            MessageBox.Show(Student.ToString());
         }
     }
 }
---
+++ Models/Student.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+
+namespace WPFLabies.Models
+{
+    public class Student : DependencyObject
+    {
+        public static readonly DependencyProperty FirstNameProperty;
+        public static readonly DependencyProperty LastNameProperty;
+        public static readonly DependencyProperty GroupProperty;
+        public static readonly DependencyProperty CourseProperty;
+
+        
+        
+        static Student()
+        {
+            FirstNameProperty = DependencyProperty.Register("FirstName", typeof(string), typeof(Student));
+            LastNameProperty = DependencyProperty.Register("LastName", typeof(string), typeof(Student));
+            GroupProperty = DependencyProperty.Register("Group", typeof(int), typeof(Student));
+            CourseProperty = DependencyProperty.Register("Course", typeof(int), typeof(Student));
+        }
+
+        public string FirstName
+        {
+            get { return (string) GetValue(FirstNameProperty); }
+            set { SetValue(FirstNameProperty, value); }
+        }
+
+        public string LastName
+        {
+            get { return (string) GetValue(LastNameProperty); }
+            set { SetValue(LastNameProperty, value); }
+        }
+
+        public int Group
+        {
+            get { return (int) GetValue(GroupProperty); }
+            set { SetValue(GroupProperty, value); }
+        }
+
+        public int Course
+        {
+            get { return (int) GetValue(CourseProperty); }
+            set { SetValue(CourseProperty, value); }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Group} {Course}";
+        }
+    }
+}
