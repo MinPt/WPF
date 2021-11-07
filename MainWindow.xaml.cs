b396517f14dcf2f87eb3add561d0bc0b656e0e83
MinPt <petr.minailyuk@gmail.com>
2021-11-07 14:32:11 +0200

    lab-6-2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Windows;
-using System.Windows.Input;
+﻿
+using WPFLabies.Models;
 
 namespace WPFLabies
 {
@@ -8,55 +8,18 @@ namespace WPFLabies
     /// </summary>
     public partial class MainWindow
     {
-        public void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        private readonly Himan _human = new Himan()
         {
-            MessageBox.Show("HELP MEEE");
-        }
-        
-        public void CommandBinding_Executed2(object sender, ExecutedRoutedEventArgs e)
-        {
-            MessageBox.Show("COPY HERE");
-        }
-        
-        public void CommandBinding_Executed3(object sender, ExecutedRoutedEventArgs e)
-        {
-            MessageBox.Show("Cut Me");
-        }
-        
-        public void CommandBinding_Executed4(object sender, ExecutedRoutedEventArgs e)
-        {
-            MessageBox.Show("Paste in HERE");
-        }
+            Name = "Petro",
+            Surname = "Minailiuk",
+            Age = 19,
+        };
+        public Himan Himan => _human;
         public MainWindow()
         {
             InitializeComponent();
-            this.HelpItem.Command = ApplicationCommands.Help;
             
-            CommandBinding commandBinding = new CommandBinding();
-            commandBinding.Command = ApplicationCommands.Help;
-            commandBinding.Executed += CommandBinding_Executed;
-            HelpItem.CommandBindings.Add(commandBinding);
-
-            this.CopyItem.Command = ApplicationCommands.Copy;
-            
-            CommandBinding copyBind = new CommandBinding();
-            copyBind.Command = ApplicationCommands.Copy;
-            copyBind.Executed += CommandBinding_Executed2;
-            CopyItem.CommandBindings.Add(copyBind);
-            
-            this.CutItem.Command = ApplicationCommands.Cut;
-            
-            CommandBinding cutBind = new CommandBinding();
-            cutBind.Command = ApplicationCommands.Cut;
-            cutBind.Executed += CommandBinding_Executed3;
-            CutItem.CommandBindings.Add(cutBind);
-            
-            this.PastItem.Command = ApplicationCommands.Paste;
-            
-            CommandBinding pasteBind = new CommandBinding();
-            pasteBind.Command = ApplicationCommands.Paste;
-            pasteBind.Executed += CommandBinding_Executed4;
-            PastItem.CommandBindings.Add(pasteBind);
+            DataContext = _human;
         }
     }
 }
---
+++ Models/Himan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFLabies.Models
+{
+   
+    
+    public class Himan : DependencyObject
+    {
+        public static readonly DependencyProperty NameProperty;
+        public static readonly DependencyProperty SurnameProperty;
+        public static readonly DependencyProperty AgeProperty;
+        
+        static Himan()
+        {
+            NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(Himan));
+            SurnameProperty = DependencyProperty.Register("Surname", typeof(string), typeof(Himan));
+            AgeProperty = DependencyProperty.Register("Age", typeof(int), typeof(Himan));
+        }
+        public string Name
+        {
+            get { return (string) GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+        
+        public string Surname
+        {
+            get { return (string) GetValue(SurnameProperty); }
+            set { SetValue(SurnameProperty, value); }
+        }
+        
+        public int Age
+        {
+            get { return (int) GetValue(AgeProperty); }
+            set { SetValue(AgeProperty, value); }
+        }
+        
+        public override string ToString()
+        {
+            return $"{Name} {Surname} {Age}";
+        }
+    }
+}
